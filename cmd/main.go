0dd9754fa7ae141e330fd98fa978f68f1aa9947f
sletkov <70477360+sletkov@users.noreply.github.com>
2023-05-08 20:45:16 +0300

    added consumer, main.go, .example.env and .gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	tgClient "password-storage-bot/internal/app/clients/telegram"
+	event_consumer "password-storage-bot/internal/app/consumer/event-consumer"
+	"password-storage-bot/internal/app/events/telegram"
+	"password-storage-bot/internal/app/storage/postgres"
+)
 
+const batchSize = 100
+
+func main() {
+	s, err := postgres.New(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("can't connect to storage: ", err)
+	}
+
+	eventsProcessor := telegram.New(
+		tgClient.New(os.Getenv("TG_BOT_HOST"), os.Getenv("TG_BOT_TOKEN")),
+		s,
+	)
+
+	log.Print("bot started")
+
+	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
+
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service is stopped", err)
+	}
 }
